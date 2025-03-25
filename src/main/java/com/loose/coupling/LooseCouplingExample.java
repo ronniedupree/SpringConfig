415a675c8b29bc5d5fd298b0927680a048d09897
Ronnie DuPree Jr <ronniedupree@gmail.com>
2025-03-25 01:35:25 -0700

    initial commit

---
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -0,0 +1,11 @@
+package com.loose.coupling;
+
+public class LooseCouplingExample {
+
+	public static void main(String[] args) {
+
+		UserDataProvider databaseProvider = new UserDatabaseProvider();
+		UserManager userManager = new UserManager(databaseProvider);
+		System.out.println(userManager.getUserInfo());
+	}
+}
